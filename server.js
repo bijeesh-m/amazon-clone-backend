b4b8dadd4579d0780deb1705aae0ecac70d4ebf4
Bijeesh M <bijeeshh.m@gmail.com>
2024-01-31 16:06:46 +0530

    fix

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ mongoose
   .connect(
     "mongodb+srv://bijeesh:qN1PwM8yoav5NJs6@cluster0.rqmpu.mongodb.net/amazon"
   )
-  // .connect("mongodb://localhost:27017/amazon")
   .then(console.log("db connected"))
   .catch((err) => console.log(err.message));
 
