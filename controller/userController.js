74ff4a627b4ad9ba533efa9e0506486d6c80914d
Bijeesh M <bijeeshh.m@gmail.com>
2024-01-31 19:19:39 +0530

    fix

--- controller/userController.js
+++ controller/userController.js
@@ -68,7 +68,7 @@ module.exports.login = async (req, res) => {
     const auth = await bcrypt.compare(user.password, userExist.password);
     if (auth) {
       const token = createToken(userExist._id, userExist.username);
-      res.cookie("userjwt", token, {httpOnly:false});
+      res.cookie("userjwt", token, { httpOnly: false, secure: true, sameSite: 'None' });
       res.status(200).json({
         user: userExist,
         data: "success",
